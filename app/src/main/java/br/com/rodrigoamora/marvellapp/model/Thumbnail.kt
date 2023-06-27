ff6565a6d5f667dfe99e7d0db8681532728552ad
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-06-27 03:56:25 -0300

    Adicionando o relacionamento entre Thumbnail e Character

--- app/src/main/java/br/com/rodrigoamora/marvellapp/model/Thumbnail.kt
+++ app/src/main/java/br/com/rodrigoamora/marvellapp/model/Thumbnail.kt
@@ -1,5 +1,22 @@
 package br.com.rodrigoamora.marvellapp.model
 
-data class Thumbnail(
-    var path: String
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity(
+    foreignKeys = [ForeignKey(
+        entity = Character::class,
+        parentColumns = arrayOf("id"),
+        childColumns = arrayOf("thumbnailId"),
+        onDelete = ForeignKey.CASCADE
+    )]
 )
+data class Thumbnail(
+
+    @PrimaryKey(autoGenerate = true)
+    var thumbnailId: Long,
+
+    var path: String
+): Serializable
