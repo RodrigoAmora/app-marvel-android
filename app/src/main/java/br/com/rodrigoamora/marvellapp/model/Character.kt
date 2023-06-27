e3394f3f8c5839b71632e4c7a9187cd2d2477dab
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-06-27 03:55:36 -0300

    Adicionando a anotação @PrimaryKey

--- app/src/main/java/br/com/rodrigoamora/marvellapp/model/Character.kt
+++ app/src/main/java/br/com/rodrigoamora/marvellapp/model/Character.kt
@@ -1,8 +1,14 @@
 package br.com.rodrigoamora.marvellapp.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity
 data class Character(
+    @PrimaryKey
     var id: Int,
     var name: String,
-    var description: String,
-    var thumbnail: Thumbnail
-)
+    var description: String
+    //var thumbnail: Thumbnail
+): Serializable
