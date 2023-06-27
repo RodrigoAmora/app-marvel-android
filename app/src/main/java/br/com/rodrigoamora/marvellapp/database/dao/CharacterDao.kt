c4c218d87bf85b20c28f821d5654987f22736b5c
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-06-27 03:55:10 -0300

    Criando o Dao de personagens

---
+++ app/src/main/java/br/com/rodrigoamora/marvellapp/database/dao/CharacterDao.kt
@@ -0,0 +1,27 @@
+package br.com.rodrigoamora.marvellapp.database.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.Query
+import br.com.rodrigoamora.marvellapp.model.Character
+
+@Dao
+interface CharacterDao {
+    @Query("SELECT * FROM Character")
+    fun findAll(): LiveData<List<Character>>
+
+    @Insert(onConflict = REPLACE)
+    fun save(character: Character)
+
+    @Insert(onConflict = REPLACE)
+    fun save(characters: List<Character>)
+
+    @Delete
+    fun remove(character: Character)
+
+    @Query("DELETE FROM Character")
+    fun removeAll()
+}
