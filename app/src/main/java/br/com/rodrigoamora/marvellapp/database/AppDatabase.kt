3822ebc6645b1a011267e8a19df512c0a99057be
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-06-28 17:28:54 -0300

    Criando a classe Character

--- app/src/main/java/br/com/rodrigoamora/marvellapp/database/AppDatabase.kt
+++ app/src/main/java/br/com/rodrigoamora/marvellapp/database/AppDatabase.kt
@@ -2,11 +2,16 @@ package br.com.rodrigoamora.marvellapp.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import br.com.rodrigoamora.marvellapp.database.converter.Converters
 import br.com.rodrigoamora.marvellapp.database.dao.CharacterDao
 import br.com.rodrigoamora.marvellapp.database.dao.ComicDao
 import br.com.rodrigoamora.marvellapp.model.Character
+import br.com.rodrigoamora.marvellapp.model.Comic
+import br.com.rodrigoamora.marvellapp.model.Thumbnail
 
-@Database(entities = [Character::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
+@Database(entities = [Character::class, Comic::class, Thumbnail::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun characterDao(): CharacterDao
