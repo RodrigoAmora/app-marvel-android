3a44b1a6849b131a8f009e9bc9441d58db29a456
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-07-10 05:30:13 -0300

    Chamando a tela de HQ

--- app/src/main/java/br/com/rodrigoamora/marvellapp/ui/fragment/CharacterFragment.kt
+++ app/src/main/java/br/com/rodrigoamora/marvellapp/ui/fragment/CharacterFragment.kt
@@ -1,10 +1,12 @@
 package br.com.rodrigoamora.marvellapp.ui.fragment
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.Spinner
@@ -15,6 +17,7 @@ import br.com.rodrigoamora.marvellapp.extension.loadCircleImageWithGlide
 import br.com.rodrigoamora.marvellapp.model.Character
 import br.com.rodrigoamora.marvellapp.model.Comic
 import br.com.rodrigoamora.marvellapp.ui.activity.CharacterActivity
+import br.com.rodrigoamora.marvellapp.ui.activity.ComicActivity
 
 class CharacterFragment: Fragment() {
 
@@ -25,16 +28,29 @@ class CharacterFragment: Fragment() {
 
     private lateinit var character: Character
     private lateinit var characterActivity: CharacterActivity
-
+    private lateinit var comics: List<Comic>
+    private var currentSelection: Int = 0
 
     @SuppressLint("MissingInflatedId")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.fragment_character, container, false)
 
-        ivImageCharacter = root.findViewById(R.id.iv_image_character)!!
-        tvDescriptionCharacter = root.findViewById(R.id.tv_description_character)!!
-        tvNameCharacter = root.findViewById(R.id.tv_name_character)!!
-        spComics = root.findViewById(R.id.sp_comics)!!
+        ivImageCharacter = root.findViewById(R.id.iv_image_character)
+        tvDescriptionCharacter = root.findViewById(R.id.tv_description_character)
+        tvNameCharacter = root.findViewById(R.id.tv_name_character)
+        spComics = root.findViewById(R.id.sp_comics)
+        spComics.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                currentSelection++
+                if (currentSelection > 1) {
+                    val intent = Intent(characterActivity, ComicActivity::class.java)
+                    intent.putExtra("comic", comics[position])
+                    startActivity(intent)
+                }
+            }
+        }
 
         return root
     }
@@ -46,15 +62,15 @@ class CharacterFragment: Fragment() {
         getComicsByCharacterId()
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    override fun onDestroyView() {
+        super.onDestroyView()
+        currentSelection = 0
+    }
+    override fun onStop() {
+        super.onStop()
+        currentSelection = 0
     }
 
-    override fun onResume() {
-        super.onResume()
-
-    }
     private fun recoveryActivity() {
         characterActivity = activity as CharacterActivity
     }
@@ -67,7 +83,6 @@ class CharacterFragment: Fragment() {
 
         tvDescriptionCharacter.text = character.description
         tvNameCharacter.text = character.name
-
     }
 
     private fun getComicsByCharacterId() {
@@ -75,6 +90,8 @@ class CharacterFragment: Fragment() {
     }
 
     fun populateSpinner(comics: List<Comic>) {
+        this.comics = comics
+
         val titles: MutableList<String> = mutableListOf()
         for (comic in comics) {
             titles.add(comic.title)
