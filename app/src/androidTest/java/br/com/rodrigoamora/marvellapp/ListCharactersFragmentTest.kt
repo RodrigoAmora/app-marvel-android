b9832d5d14757b39061b9840a552b052cefa6b90
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-12-17 21:05:01 -0300

    Implementando o método de teste de busca de personagem por nome

--- app/src/androidTest/java/br/com/rodrigoamora/marvellapp/ListCharactersFragmentTest.kt
+++ app/src/androidTest/java/br/com/rodrigoamora/marvellapp/ListCharactersFragmentTest.kt
@@ -1,9 +1,11 @@
 package br.com.rodrigoamora.marvellapp
 
 import android.content.Context
+import android.view.KeyEvent
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -23,15 +25,7 @@ class ListCharactersFragmentTest {
     @Before
     fun init() {
         ActivityScenario.launch(MainActivity::class.java)
-        context = InstrumentationRegistry.getInstrumentation().targetContext
-    }
-
-    @Test
-    fun testShouldShowAppName() {
-        Espresso
-            .onView(ViewMatchers.withId(R.id.nav_view))
-            .check(ViewAssertions.matches(ViewMatchers.withText(context.getString(R.string.app_name))))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        this.context = InstrumentationRegistry.getInstrumentation().targetContext
     }
 
     @Test
@@ -47,18 +41,39 @@ class ListCharactersFragmentTest {
     }
 
     @Test
-    fun testClickOnItemRecyclerViewAndCheckNameCharacterInCharacterFragment() {
+    fun testSearchCharacterByName() {
         Espresso
-            .onView(RecyclerViewMatcher.recyclerViewWithId(R.id.list_characters).itemViewAtIndex(0))
+            .onView(ViewMatchers.withId(R.id.list_characters))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        Espresso
+            .onView(RecyclerViewMatcher.recyclerViewWithId(R.id.list_characters).viewHolderViewAtPosition(0, R.id.tv_name_character_value))
+            .check(ViewAssertions.matches(ViewMatchers.withText("3-D Man")))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        Espresso
+            .onView(ViewMatchers.withId(R.id.fab_search_character_by_name))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
             .perform(ViewActions.click())
 
         Espresso
-            .onView(ViewMatchers.withId(R.id.tv_name_character))
+            .onView(ViewMatchers.withId(R.id.sv_search_character_by_name))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            .perform(ViewActions.click())
+            .perform(ViewActions.typeText("Hulk"))
+            .perform(ViewActions.pressKey(KeyEvent.KEYCODE_ENTER), closeSoftKeyboard())
+
+        Thread.sleep(7000L)
+
+        Espresso
+            .onView(RecyclerViewMatcher.recyclerViewWithId(R.id.list_characters).viewHolderViewAtPosition(0, R.id.tv_name_character_value))
+            .check(ViewAssertions.matches(ViewMatchers.withText("Hulk")))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
 
         Espresso
-            .onView(ViewMatchers.withId(R.id.tv_name_character))
-            .check(ViewAssertions.matches(ViewMatchers.withText("3-D Man")))
+            .onView(RecyclerViewMatcher.recyclerViewWithId(R.id.list_characters).viewHolderViewAtPosition(1, R.id.tv_name_character_value))
+            .check(ViewAssertions.matches(ViewMatchers.withText("Hulk (HAS)")))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
+
 }
